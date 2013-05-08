5c02cae79e714c61232349deb240c66aea926e7e
Chintan Patel <chintu890@gmail.com>
2013-05-07 21:32:23 -0500

    Initial GalleryFragment class creation

---
+++ src/com/chintanpatel/wiwac/GalleryFragment.java
@@ -0,0 +1,24 @@
+package com.chintanpatel.wiwac;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+
+public class GalleryFragment extends Fragment implements OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.gallery_fragment, container, false);
+        return view;
+    }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
