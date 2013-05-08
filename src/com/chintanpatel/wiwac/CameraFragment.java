5abf7cfd27eea3e76fd522f2b633891f15bd1bc2
Chintan Patel <chintu890@gmail.com>
2013-05-07 22:47:51 -0500

    Now opens camera app via intent to takes picture

--- src/com/chintanpatel/wiwac/CameraFragment.java
+++ src/com/chintanpatel/wiwac/CameraFragment.java
@@ -1,6 +1,7 @@
 package com.chintanpatel.wiwac;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +10,14 @@ import android.view.ViewGroup;
 
 public class CameraFragment extends Fragment implements OnClickListener {
 
+	private static final int CAMERA_PIC_REQUEST = 1337;  
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.camera_fragment, container, false);
+        
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST);
         return view;
     }
 
@@ -20,7 +26,5 @@ public class CameraFragment extends Fragment implements OnClickListener {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
 
 }
