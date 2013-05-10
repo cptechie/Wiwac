e9a54c29624437066049abac7abb0e34e698ca26
Chintan Patel <chintu890@gmail.com>
2013-05-10 01:00:00 -0500

    Camera now saves to /Picture/Wiwac and displays in ImageView with resize

--- src/com/final_proj/patel/CameraFragment.java
+++ src/com/final_proj/patel/CameraFragment.java
@@ -2,12 +2,10 @@ package com.final_proj.patel;
 
 import java.io.File;
 
-import com.chintanpatel.wiwac.R;
-
 import android.app.Fragment;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -18,83 +16,47 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import com.chintanpatel.wiwac.R;
 
 public class CameraFragment extends Fragment implements OnClickListener {
 
+	private File file;
+	private String path;
 	private Uri mImageUri;
 	private View view;
 	private static final int CAMERA_EVENT = 2345;
 	 
-    @Override
+	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.camera_fragment, container, false);
         
-        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-        File photo;
-        try
-        {
-            // place where to store camera taken picture
-            photo = this.createTemporaryFile("picture", ".jpg");
-            photo.delete();
-        }
-        catch(Exception e)
-        {
-            Log.v("CameraFragment", "Can't create file to take picture!");
-            Toast.makeText(getActivity(), "Please check SD card! Image shot is impossible!", Toast.LENGTH_LONG).show();
-            return null;
-        }
-        mImageUri = Uri.fromFile(photo);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-        //start camera intent
-        startActivityForResult(intent, CAMERA_EVENT);
+        startCamera(); 
+
         
         return view;
     }
-    
-    private File createTemporaryFile(String part, String ext) throws Exception
-    {
-        File tempDir= Environment.getExternalStorageDirectory();
-        tempDir=new File(tempDir.getAbsolutePath()+"/.temp/");
-        if(!tempDir.exists())
-        {
-            tempDir.mkdir();
-        }
-        return File.createTempFile(part, ext, tempDir);
-    }
 
+	public void startCamera() {
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        path = Environment.getExternalStorageDirectory()+"/Wiwac";
+        File directory = new File(path);
+        directory.mkdirs();
+        file = new File(path, String.valueOf(System.currentTimeMillis()) + ".jpg"); 
+         
+        mImageUri = Uri.fromFile(file); 
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri); 
+         
+        startActivityForResult(intent, CAMERA_EVENT);
+	}
+    
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	public void grabImage(ImageView imageView)
-	{
-		Log.d("CameraFragment", "Inside grabImage");
-		
-		getActivity().getContentResolver().notifyChange(mImageUri, null);
-	    ContentResolver cr = getActivity().getContentResolver();
-	    
-	    Bitmap bitmap;
-	    try
-	    {
-	    	Log.d("CameraFragment", "Inside try block to getBitmap");
-	    	
-	        bitmap = android.provider.MediaStore.Images.Media.getBitmap(cr, mImageUri);
-	        imageView.setImageBitmap(bitmap);
-	        
-	        Log.d("CameraFragment", "Image Set");
-	    }
-	    catch (Exception e)
-	    {
-	        Toast.makeText(getActivity(), "Failed to load", Toast.LENGTH_SHORT).show();
-	        Log.d("CameraFragment", "Failed to load", e);
-	    }
-	}
 
 
-	//called after camera intent finished
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent intent)
 	{
@@ -103,10 +65,14 @@ public class CameraFragment extends Fragment implements OnClickListener {
 	    if(requestCode==CAMERA_EVENT)
 	    {
 	    	Log.d("CameraFragment", "Inside requestCode==CAMERA_EVENT");
-	    	
+	    	Log.d("CameraFragment", "ImageLocation: " + mImageUri.toString());
 			ImageView imageView = (ImageView) view.findViewById(R.id.imageview_pic_holder);
-			//... some code to inflate/create/find appropriate ImageView to place grabbed image
-			this.grabImage(imageView);
+			
+			BitmapFactory.Options options = new BitmapFactory.Options();
+			options.inSampleSize=16;
+			Bitmap bmp=BitmapFactory.decodeFile(mImageUri.getPath(), options);
+			
+			imageView.setImageBitmap(bmp);
 	    }
 	    
 	    super.onActivityResult(requestCode, resultCode, intent);
